f90ea684fbba7c08d05bb5f762bfefaf5825edb9
Pete Maynard <pete@port22.co.uk>
2024-05-01 16:46:38 +0100

    Use JSON API (#8)

---
+++ assets/js/main.js
@@ -0,0 +1,65 @@
+"use strict"; 
+
+const api = axios.create({
+  baseURL: 'http://localhost:8088',
+  timeout: 1000,
+});
+
+switch (window.location.pathname.split( '/' )[1]) {
+case 'register':
+  api.post('/request', {
+      Email: 'pete@port22.co.uk'
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    break;
+case 'access':
+  access();
+  break;
+case 'refresh':
+  refresh();
+  break;
+};
+
+function access() {
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+
+  api.get('/access', {
+      params: {
+        otc: params.otc
+      }
+    })
+    .then(function (response) {
+      document.getElementById('#textconfig').innerHTML = response.data.Config;
+      document.getElementById('#pngconfig').src += response.data.ConfigPNG;
+      document.getElementById('#fileconfig').href += response.data.Config64;
+    })
+    .catch(function (error) {
+      window.location.replace("/");
+    });
+}
+
+
+function refresh() {
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+
+  api.get('/refresh', {
+      params: {
+        otc: params.otc
+      }
+    })
+    .then(function (response) {
+      window.location.replace("/access?otc="+response.data.OTC);
+    })
+    .catch(function (error) {
+      window.location.replace("/");
+    });
+}
---
+++ content/access.md
@@ -0,0 +1,12 @@
+---
+title: Access
+--- 
+
+Find your connection details below.
+
+By default [AdGuard](https://adguard.com/en/welcome.html) public DNS settings are used, and all traffic is routed through the tunnel
+
+<img id="#pngconfig" style="float:right" src="data:image/png;base64, " alt="QR Config"/>
+<pre id="#textconfig"></pre>
+
+<a id="#fileconfig" download="vpn.conf" href="data:application/plaintext;base64,">Download</a>
